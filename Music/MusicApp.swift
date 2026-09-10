917117af6fbcfc694dcd783239ff04218380c388
karunakark234 <karunakark234@gmail.com>
2026-09-10 14:34:25 -0400

    Initial Commit

---
+++ Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by Karunakar Katraju on 9/10/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
